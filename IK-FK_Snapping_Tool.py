6663051f66c04358d785b0d6ed5272bd9cd42808
Endertainer007 <57242521+Endertainer@users.noreply.github.com>
2022-10-06 07:56:25 +0700

    Update IK-FK_Snapping_Tool.py

--- IK-FK_Snapping_Tool.py
+++ IK-FK_Snapping_Tool.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2022 Byron Mallett
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import bpy
 from mathutils import Matrix, Vector
 from bl_ui.utils import PresetPanel
@@ -10,7 +32,7 @@ bl_info = {
     'category': 'Animation',
     'location': 'View3D > Sidebar > IK-FK Snap',
     'version': (1, 1, 0),
-    'author': 'Byron Mallett (Edited by Endertainer007)',
+    'author': 'Byron Mallett (Modified by Endertainer007)',
     'warning': '',
     'description': 'Tools to perform IK to FK and FK to IK snapping',
 }
